618446624fc8cbe381ea4f84dd78d8123dee56ac
Ashleigh Edwards <ashleigh.edwards@mhr.co.uk>
2025-11-02 22:36:26 +0000

    Added sorting and pagination

--- store/static/store/js/cart.js
+++ store/static/store/js/cart.js
@@ -40,3 +40,15 @@ function getCSRFToken() {
     }
     return cookieValue;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sortSelect = document.getElementById('sort-select');
+    if (sortSelect) {
+        sortSelect.addEventListener('change', function() {
+            const selectedSort = this.value;
+            const url = new URL(window.location.href);
+            url.searchParams.set('sort', selectedSort);
+            window.location.href = url.toString();
+        });
+    }
+});
--- store/views.py
+++ store/views.py
@@ -1,15 +1,43 @@
+from django.core.paginator import Paginator
+from django.db.models import Case, When, Value, BooleanField
 from django.http import JsonResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 #this view fetches all products from the database and sends them to a template
 def product_list(request):
-    # Get all Product objects
-    products = Product.objects.all()
+    sort = request.GET.get('sort', 'name')  # default sort by name
+
+    products = Product.objects.annotate(
+        is_out_of_stock=Case(
+            When(stock__lte=0, then=Value(True)),
+            default=Value(False),
+            output_field=BooleanField()
+        )
+    )
+
+    if sort == 'price_asc':
+        products = products.order_by('is_out_of_stock', 'price')
+    elif sort == 'price_desc':
+        products = products.order_by('is_out_of_stock', '-price')
+    elif sort == 'name_asc':
+        products = products.order_by('is_out_of_stock', 'name')
+    elif sort == 'name_desc':
+        products = products.order_by('is_out_of_stock', '-name')
+    else:
+        products = products.order_by('is_out_of_stock', 'name')  # default
 
     #render the product list template
     #pass the products as a context dictionary
-    return render(request, 'store/product_list.html', {'products': products})
+    paginator = Paginator(products, 36)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'store/product_list.html', {
+        'page_obj': page_obj,
+        'products': page_obj.object_list,
+        'current_sort': sort,
+    })
 
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
